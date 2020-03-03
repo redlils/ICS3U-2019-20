c17e5908fdadacb659a35dcb5b696ee1b14ab780
Kalcoder <kalebr82@kprschools.ca>
2020-03-03 14:30:54 -0500

    Create base form

---
+++ U1A5/Mutliple Choice Quiz/src/me/Kaleb/MultipleChoiceQuiz.java
@@ -0,0 +1,25 @@
+package me.Kaleb;
+
+import javax.swing.*;
+
+public class MultipleChoiceQuiz {
+  //<editor-fold desc="Auto-generated GUI components">
+  private JPanel pnlMain;
+  private JTabbedPane tbPnlMain;
+  private JPanel pnlQuestion1;
+  private JPanel pnlQuestion2;
+  private JPanel pnlQuestion3;
+  private JPanel pnlQuestion4;
+  private JPanel pnlQuestion5;
+  private JTextField textField1;
+  //</editor-fold>
+  
+  
+  public static void main(String[] args) {
+    JFrame frame = new JFrame("MultipleChoiceQuiz");
+    frame.setContentPane(new MultipleChoiceQuiz().pnlMain);
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    frame.pack();
+    frame.setVisible(true);
+  }
+}

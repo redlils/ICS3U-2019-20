3e62c6f0a83bd0d8e8597b39cc2e4113937ffb09
Kalcoder <kalebr82@kprschools.ca>
2020-03-11 14:30:41 -0400

    Start work on choose your own adventure game

--- U2A1/Choose Your Own Adventure/src/me/Kaleb/MainClass.java
+++ U2A1/Choose Your Own Adventure/src/me/Kaleb/MainClass.java
@@ -1,4 +1,26 @@
 package me.Kaleb;
 
+import java.util.Scanner;
+
 public class MainClass {
+  
+  public static void main(String[] args) {
+    Scanner reader = new Scanner(System.in);
+    
+  }
+  
+  private static boolean confirm(String prompt, Scanner reader) {
+    System.out.print(prompt + " [y/n]: ");
+    while (!reader.hasNext("[yY|nN]")) {
+      System.out.print(prompt + " [y/n]: ");
+      reader.next();
+    }
+    
+    return reader.next().equalsIgnoreCase("y");
+  }
+  
+  private static T getSelectionFromList<T>() {
+  
+  }
+  
 }

b473864b3213a963c732c570980570f14eb65c0d
Kalcoder <kalebr82@kprschools.ca>
2020-03-25 16:27:04 -0400

    Add JavaDoc to classes in Helpers module

--- Helpers/src/me/Kaleb/InputUtils.java
+++ Helpers/src/me/Kaleb/InputUtils.java
@@ -9,7 +9,14 @@ import java.util.List;
 import java.util.Scanner;
 
 public class InputUtils {
-  //  Confirm selection/operation with user
+  
+  /**
+   * Asks for confirmation from user
+   *
+   * @param prompt The prompt to give the user
+   * @param reader The scanner to recieve input from
+   * @return The user's answer as a boolean
+   */
   public static boolean confirm(String prompt, Scanner reader) {
     System.out.print(prompt + " [y/n]: ");
     while (!reader.hasNext("[yY|nN]")) {
@@ -20,7 +27,15 @@ public class InputUtils {
     return reader.next().equalsIgnoreCase("y");
   }
   
-  //  Get selection from provided list
+  /**
+   * Gets a selection by the user from a list
+   *
+   * @param prompt The prompt to give the user
+   * @param reader The scanner to receive input from
+   * @param items The list of items for the user to choose from
+   * @param <T> The return type and type of list
+   * @return The selected item
+   */
   public static <T> T getSelectionFromList(String prompt, Scanner reader, List<T> items) {
 //    Create StringBuilder for better concatenation
     StringBuilder output = new StringBuilder(prompt + " [");
@@ -47,12 +62,27 @@ public class InputUtils {
     return items.get(reader.nextInt() - 1);
   }
   
-  //  Used to translate array into list for getSelectionFromList
+  /**
+   * Gets a selection by the user from a list
+   *
+   * @param prompt The prompt to give the user
+   * @param reader The scanner to receive input from
+   * @param items The list of items for the user to choose from
+   * @param <T> The return type and type of list
+   * @return The selected item
+   */
   @SafeVarargs public static <T> T getSelectionFromList(String prompt, Scanner reader, T... items) {
     return getSelectionFromList(prompt, reader, Arrays.asList(items));
   }
   
-  //  Get index from selection from list
+  /**
+   * Get the index of the user's selection in a list
+   *
+   * @param prompt The prompt to give to the user
+   * @param reader The scanner to receive input from
+   * @param items The list of items for the user to choose from
+   * @return The index of the selected item
+   */
   public static int getIndexFromSelection(String prompt, Scanner reader, List<?> items) {
 //    Create StringBuilder for better concatenation
     StringBuilder output = new StringBuilder(prompt + " [");
@@ -65,8 +95,6 @@ public class InputUtils {
               .append(item.toString())
               .append((i + 1 == items.size()) ? "]: " : ", "); // Add tuple to change concatenation if item is last in list
     }
-
-//    Print the list of items and the prompt
     System.out.print(output);
 
 //    Verify input
@@ -78,7 +106,14 @@ public class InputUtils {
     return reader.nextInt() - 1;
   }
   
-  //  Translate array into list for getIndexFromSelection
+  /**
+   * Get the index of the user's selection in a list
+   *
+   * @param prompt The prompt to give to the user
+   * @param reader The scanner to receive input from
+   * @param items The list of items for the user to choose from
+   * @return The index of the selected item
+   */
   public static int getIndexFromSelection(String prompt, Scanner reader, Object... items) {
     return getIndexFromSelection(prompt, reader, Arrays.asList(items));
   }
--- Helpers/src/me/Kaleb/NumberUtils.java
+++ Helpers/src/me/Kaleb/NumberUtils.java
@@ -2,6 +2,13 @@ package me.Kaleb;
 
 public class NumberUtils {
   
+  /**
+   * Round a double
+   *
+   * @param numberToRound The number to round
+   * @param decimalPlaces The amount of decimal places to round to
+   * @return The rounded number
+   */
   public static double round(double numberToRound, int decimalPlaces) {
     return Math.round(Math.pow(10, decimalPlaces) * numberToRound) / Math.pow(10, decimalPlaces);
   }

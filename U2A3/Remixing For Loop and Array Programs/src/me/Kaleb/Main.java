6bf12bf75d810affb3c3ccd3ba2f2e34ddaf5f3b
Kalcoder <kalebr82@kprschools.ca>
2020-03-31 15:23:15 -0400

    Clean up code
    Add comments, simplify code, remove unnecessary white-space

--- U2A3/Remixing For Loop and Array Programs/src/me/Kaleb/Main.java
+++ U2A3/Remixing For Loop and Array Programs/src/me/Kaleb/Main.java
@@ -11,66 +11,81 @@ public class Main {
   }
   
   private static void runProgramOne() {
+    // Creates Scanner for input
     Scanner keyedInput = new Scanner(System.in);
   
+    // Declare and initialize the array to store inputted numbers and the total of all numbers
     int[] numbers = new int[10];
     int total = 0;
   
+    // Prompt user for ten integers to be added
     System.out.println("Enter ten integers and they will be added together:");
-    for (int i = 0; i <= 9; i = i + 1)
+    for (int i = 0; i < numbers.length; i++)
     {
+      // Add inputted number to numbers[]
       numbers[i] = keyedInput.nextInt();
     }
   
-    for (int i = 0; i <= 9; i = i + 1)
+    // Loop through all values in numbers[] and add them together
+    for (int i = 0; i < numbers.length; i++)
     {
       total = total + numbers[i];
     }
   
+    // Print total of numbers in numbers[]
     System.out.println("The sum of those numbers is:");
     System.out.println(total);
   }
   
   private static void runProgramTwo() {
-    Scanner keyedInput = new Scanner (System.in);
+    // Creates Scanner for input
+    Scanner keyedInput = new Scanner(System.in);
   
-    String [ ] friends = new String [10];
+    // Declare and initialize an array of 10 strings called friends
+    String[] friends = new String[10];
   
+    // Prompt user for 10 names of friends
     System.out.println("Enter the names of ten friends:");
-    for (int i = 0; i <= 9; i = i + 1)
+    for (int i = 0; i < friends.length; i++)
     {
       friends[i] = keyedInput.nextLine();
     }
   
+    // Print 4th and 7th inputted names
     System.out.println("The fourth and seventh names listed were:");
     System.out.println("Fourth: " + friends[3]);
     System.out.println("Seventh: " + friends[6]);
   }
   
   private static void runProgramThree() {
-    Scanner keyedInput = new Scanner (System.in);
+    // Creates Scanner for input
+    Scanner keyedInput = new Scanner(System.in);
   
-    double [ ] marks = {34.7, 54.1, 34.8, 99.6, 43.6, 43.2};
+    // Declare and initialize an array of doubles called marks, a double to hold the total of all values in marks[], and a double to hold average of all values in marks[]
+    double[] marks = {34.7, 54.1, 34.8, 99.6, 43.6, 43.2};
     double total=0;
     double average;
   
+    // Print out all marks
     System.out.println("These are the marks:");
-    for (int i = 0; i<=5; i= i + 1)
+    for (int i = 0; i < marks.length; i++)
     {
       System.out.println(marks[i]);
     }
   
-    for (int i = 0; i<=5; i= i + 1)
+    // Add all marks together
+    for (int i = 0; i < marks.length; i++)
     {
       total = total + marks[i];
     }
   
+    // Calculate average of all marks and store it in average variable
     average = total/6;
-  
-    average = average * 10;
+    average *= 10;
     average = Math.round(average);
-    average = average/10;
+    average /= 10;
   
+    // Print out average
     System.out.println("The average mark is:");
     System.out.println(average);
   }

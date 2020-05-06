1398e1d40bb027be795f6df5c772222adc9d424e
Kaleb <kalebr82@kprschools.ca>
2020-05-06 15:58:19 -0400

    Convert Flowchart outline into Comment outline

--- U2/A6/Large Software Development/src/me/Kaleb/Main.java
+++ U2/A6/Large Software Development/src/me/Kaleb/Main.java
@@ -1,4 +1,42 @@
 package me.Kaleb;
 
 public class Main {
+  
+  public static void main(String[] args) {
+    // Output welcome message
+    
+    // Infinite loop
+    
+      // Prompt for command
+      
+      // Get command from input
+      
+      // Does the command start with the prefix
+    
+      // Yes:
+    
+        // Loop through all registered command
+          
+          // Does the current command match the inputted command?
+          
+          // Yes:
+      
+            // Execute the command (exit command will handle itself)
+      
+          // No:
+      
+            // Is this the last item in the list?
+      
+            // Yes:
+      
+              // Output invalid command message and start the process over again
+      
+            // No:
+      
+              // Select the next command from the registered commands
+    
+        // No:
+    
+          // Output invalid command message and start the process over again
+  }
 }

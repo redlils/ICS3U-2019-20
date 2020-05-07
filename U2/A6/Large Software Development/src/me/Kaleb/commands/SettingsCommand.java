9835dc42cf79cb2693f2c677412ba1a084aed2b3
Kaleb <kalebr82@kprschools.ca>
2020-05-07 13:43:47 -0400

    [Command] Create base for "settings"

---
+++ U2/A6/Large Software Development/src/me/Kaleb/commands/SettingsCommand.java
@@ -0,0 +1,13 @@
+package me.Kaleb.commands;
+
+public class SettingsCommand extends Command{
+  
+  public SettingsCommand() {
+    super("settings");
+  }
+  
+  @Override
+  public void execute() {
+    
+  }
+}

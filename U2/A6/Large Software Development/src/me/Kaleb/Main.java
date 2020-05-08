dbb87375b409871908474381c7bdf10a2ef21fb7
Kaleb <kalebr82@kprschools.ca>
2020-05-08 14:18:24 -0400

    [Fix] Prompt for commands uses prefix setting value

--- U2/A6/Large Software Development/src/me/Kaleb/Main.java
+++ U2/A6/Large Software Development/src/me/Kaleb/Main.java
@@ -46,7 +46,7 @@ public class Main {
     //noinspection InfiniteLoopStatement
     while (true) {
       // Prompt for command
-      System.out.print("Please enter a command (you can type " + Settings.getSetting("prefix") + "help to get a list of commands): ");
+      System.out.print("Please enter a command (you can type " + Settings.getSetting("prefix").getValue() + "help to get a list of commands): ");
       
       // Get command from input
       commandInput = reader.nextLine();

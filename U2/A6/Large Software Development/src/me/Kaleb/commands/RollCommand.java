afa0af8db0b290edb14b60f59280bcafddf82dc5
Kaleb <kalebr82@kprschools.ca>
2020-05-10 11:30:24 -0400

    [Fix] Guessing now has checks in place
    You are now limited to 1-6 for guessing numbers
    Output message fixed

--- U2/A6/Large Software Development/src/me/Kaleb/commands/RollCommand.java
+++ U2/A6/Large Software Development/src/me/Kaleb/commands/RollCommand.java
@@ -1,5 +1,7 @@
 package me.Kaleb.commands;
 
+import me.Kaleb.NumberUtils;
+
 import java.util.Random;
 import java.util.Scanner;
 
@@ -22,7 +24,12 @@ public class RollCommand extends Command {
   public void execute() {
     Scanner reader = new Scanner(System.in);
     System.out.print("Please enter what you think your next roll will be: ");
-    int guess = Integer.parseInt(reader.next("[1-6]"));
+    
+    while (!reader.hasNext("[1-6]")) {
+      System.out.print("Please enter what you think your next roll will be: ");
+      reader.next();
+    }
+    int guess = reader.nextInt();
     
     Random random = new Random();
     int roll = random.nextInt(6) + 1;
@@ -33,6 +40,6 @@ public class RollCommand extends Command {
     System.out.println("You rolled a " + roll + "!");
     System.out.println("Your guess was " + (guess == roll ? "correct!" : "incorrect."));
     System.out.println("You have guessed a total of " + correct + " rolls correctly.");
-    System.out.println("Your percentage of correctly guessed rolls is " + correct / rolls);
+    System.out.println("Your percentage of correctly guessed rolls is " + NumberUtils.round((double) correct / rolls, 3) * 100 + "%");
   }
 }

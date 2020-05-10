6a7da8dad8a44336fba0b53be81ddc9aa7754847
Kaleb <kalebr82@kprschools.ca>
2020-05-10 11:05:24 -0400

    [Change] Add guessing to rolls

--- U2/A6/Large Software Development/src/me/Kaleb/commands/RollCommand.java
+++ U2/A6/Large Software Development/src/me/Kaleb/commands/RollCommand.java
@@ -1,13 +1,15 @@
 package me.Kaleb.commands;
 
 import java.util.Random;
+import java.util.Scanner;
 
 public class RollCommand extends Command {
   
+  private int correct;
+  private int rolls;
+  
   /**
    * Constructor
-   *
-   * @param name The name of the command
    */
   public RollCommand() {
     super("roll");
@@ -18,8 +20,19 @@ public class RollCommand extends Command {
    */
   @Override
   public void execute() {
+    Scanner reader = new Scanner(System.in);
+    System.out.print("Please enter what you think your next roll will be: ");
+    int guess = Integer.parseInt(reader.next("[1-6]"));
+    
     Random random = new Random();
     int roll = random.nextInt(6) + 1;
+    
+    rolls++;
+    correct += guess == roll ? 1 : 0;
+    
     System.out.println("You rolled a " + roll + "!");
+    System.out.println("Your guess was " + (guess == roll ? "correct!" : "incorrect."));
+    System.out.println("You have guessed a total of " + correct + " rolls correctly.");
+    System.out.println("Your percentage of correctly guessed rolls is " + correct / rolls);
   }
 }
